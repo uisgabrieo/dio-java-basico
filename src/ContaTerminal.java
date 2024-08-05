f54eb25691263a3d89838eeb75847d208fbc69da
Luis Gabriel <barros.luissalv@gmail.com>
2024-08-05 19:04:26 -0300

    Classe Conta criada

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,6 +1,51 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class ContaTerminal {
+class Conta {
+    private Integer numero;
+    private String agencia;
+    private String nomeCliente;
+    private Double saldo;
+
+    public Conta() {}
+    public Conta(Integer numero, String agencia, String nomeCliente, Double saldo) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.nomeCliente  = nomeCliente;
+        this.saldo = saldo;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+}
+
+public class ContaTerminal{
     public static void main(String[] args) {
 
     }
